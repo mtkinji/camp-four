ec92ddd4cc1c17137b49bc7b1695d3f385bc91a9
Andrew Watanabe <mtkinji@gmail.com>
2018-12-17 15:37:25 -0700

    added timestamps to userSchema documents

--- server.js
+++ server.js
@@ -7,10 +7,20 @@ const port = 3000;
 const dbURL = mongoose.connect('mongodb+srv://user123:user123@camp-four-ikqzz.mongodb.net/camp-four?retryWrites=true',{ useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
-  fname: { type: String, required: true },
-  lname: { type: String, required: true },
-  email: { type: String, required: true },
-  created: { type: Date, default: Date.now },
+  fname:    { type: String, required: true },
+  lname:    { type: String, required: true },
+  email:    { type: String, required: true },
+  team:     { type: String},
+},
+{ timestamps: { createdAt: 'created_at' } }
+);
+
+const objectiveSchema = new mongoose.Schema({
+  objective:    { type: String, required: true },
+  type:         { type: String, required: true },
+  email:    { type: String, required: true },
+  team:     { type: String, required: true},
+  created:  { type: Date, default: Date.now }
 });
 
 userSchema.methods.speak = function () {
@@ -19,6 +29,7 @@ userSchema.methods.speak = function () {
     : "I don't have a name";
   console.log(greeting);
 }
+
 const User = mongoose.model('User', userSchema);
 
 var createdUser = new User({
