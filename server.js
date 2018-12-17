d7de0c881c60700af3002304f47760ff318c5739
Andrew Watanabe <mtkinji@gmail.com>
2018-12-17 15:22:14 -0700

    added initial user schema and the ability to push a hard coded user into the DB

--- server.js
+++ server.js
@@ -4,20 +4,38 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-const dbURL = mongoose.connect('mongodb+srv://user123:user123@camp-four-ikqzz.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true });
+const dbURL = mongoose.connect('mongodb+srv://user123:user123@camp-four-ikqzz.mongodb.net/camp-four?retryWrites=true',{ useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema({
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
+  email: { type: String, required: true },
+  created: { type: Date, default: Date.now },
+});
+
+userSchema.methods.speak = function () {
+  var greeting = this.fname
+    ? "My name is " + this.fname + " " + this.lname
+    : "I don't have a name";
+  console.log(greeting);
+}
+const User = mongoose.model('User', userSchema);
+
+var createdUser = new User({
+  fname: 'Blaire', lname: 'Whitaker', email: 'bwhita@gmail.com'});
+console.log(createdUser);
+
+createdUser.save(function (err, andy) {
+  if (err) return console.error(err);
+  createdUser.speak();
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("Connected to MongoDB Atlas")
 });
-
-var kittySchema = new mongoose.Schema({
-  name: String
-});
-
-var Kitten = mongoose.model('Kitten', kittySchema);
