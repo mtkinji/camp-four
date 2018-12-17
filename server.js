f70707612af1c5be1824db26248971d2ee201466
Andrew Watanabe <mtkinji@gmail.com>
2018-12-17 14:53:49 -0700

    added GET and POST method routes from express documentation

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const dbURL = mongoose.connect('mongodb+srv://user123:user123@camp-four-ikqzz.mongodb.net/test?retryWrites=true');
-
 const app = express()
 const port = 3000
 
+const dbURL = mongoose.connect('mongodb+srv://user123:user123@camp-four-ikqzz.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+// GET method route
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage')
+})
+
+// POST method route
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
