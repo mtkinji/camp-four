22129a79fb74e4d9fccafce5d128404856a028bf
Andrew Watanabe <mtkinji@gmail.com>
2018-12-17 14:57:47 -0700

    added test schema to test connnection to mongodb atlas

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-const dbURL = mongoose.connect('mongodb+srv://user123:user123@camp-four-ikqzz.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+const dbURL = mongoose.connect('mongodb+srv://user123:user123@camp-four-ikqzz.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-// GET method route
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage')
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-// POST method route
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
-})
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to MongoDB Atlas")
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var kittySchema = new mongoose.Schema({
+  name: String
+});
 
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
+var Kitten = mongoose.model('Kitten', kittySchema);
